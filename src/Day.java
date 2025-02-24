f4a7a5f6df857ebf1bf8da938df47b7cc9fc704d
chandrashekharbhaupande <chpande55@gmail.com>
2025-02-24 22:48:34 +0530

    enum changes

--- src/Day.java
+++ src/Day.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Arrays;
+
 enum DayEnum{
     SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
 }
@@ -7,6 +9,7 @@ public class Day {
 
     public static void main(String[] args) {
         DayEnum today = DayEnum.WEDNESDAY;
+        System.out.println(Arrays.stream(DayEnum.values()).toList());
         System.out.println("Today is: " + today); // Output: Today is: WEDNESDAY
     }
 }
